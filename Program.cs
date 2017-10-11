5b71a1bb3e5fd96401eb3f235d16262ffc3e85d6
HOYU <hoyu_z@163.com>
2017-10-11 17:20:57 +0800

    添加代码

--- Program.cs
+++ Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
 
 namespace TXTCtrl
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Read();
         }
+
+        //需要指定文件路径（包括文件名称）
+        public static List<string[]> Read()
+        {
+            string path = "e://20171010.txt";
+            FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read);
+            string line = string.Empty;
+            StreamReader sr = new StreamReader(fs, Encoding.Default);
+            List<string[]> list = new List<string[]>();
+            while ((line = sr.ReadLine()) != null)
+            {
+                string s = line;
+                if (s.Contains("Request"))
+                {
+                    string[] strArray = s.Split(" ");
+                    list.Add(strArray);
+                }
+            }
+            return list;
+        }
+
+        public static void Ctrl(List<string[]> list)
+        {
+                        
+        }
+
     }
+
+
 }
